4fc8fb4985e12f3334654b19aa86e8ac1fbf557b
deepanshu-2021 <deepanshubaluni07@gmail.com>
2024-08-13 15:28:57 +0530

    minor fix in backend

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,9 @@ connectDB();
 app.use("/api/products", productRouter);
 app.use("/api/user", userRouter);
 app.use("/api/orders", orderRouter);
-const _dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname + "/frontend/build")));
-  //any route that is not presetn will go to index.html
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("*", (req, res) => {
-    res.send("hi mom");
-  });
-}
+app.get("*", (req, res) => {
+  res.send("hi mom");
+});
 app.use(notFound);
 app.use(erorrHandler);
 app.listen(port, () => {
