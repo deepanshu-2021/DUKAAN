b05cd9a52d336635b265467d216a308df45ec22a
deepanshu-2021 <deepanshubaluni07@gmail.com>
2024-08-14 14:26:36 +0530

    addding the base url

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,5 +1,4 @@
-export const BASE_URL = "/api";
+export const BASE_URL = "https://dukaan-qcwm.onrender.com/api";
 export const PRODUCTS_URL = "/products";
 export const USERS_URL = "/user";
 export const ORDERS_URL = "/orders";
-export const PAYPAL_URL = "/config/paypal";
