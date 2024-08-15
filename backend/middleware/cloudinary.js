82dbf2dfdf66f69d6348bc5c205c978d7b320535
deepanshu-2021 <deepanshubaluni07@gmail.com>
2024-08-15 13:20:29 +0530

    multer image folder path correction

--- backend/middleware/cloudinary.js
+++ backend/middleware/cloudinary.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 import dotenv from "dotenv";
-import asyncHandler from "./asyncHandler.js";
 dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
