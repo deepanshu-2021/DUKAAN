4e224f2b452ccc41bd46b00b895b6c50cb327b7f
deepanshu-2021 <deepanshubaluni07@gmail.com>
2024-08-15 13:34:02 +0530

    public image folder

--- backend/middleware/multerMiddleware.js
+++ backend/middleware/multerMiddleware.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images/");
+    cb(null, "./backend/public/images/");
   },
   filename: function (req, file, cb) {
     const prfix = Date.now() + Math.random() * 1e5;
